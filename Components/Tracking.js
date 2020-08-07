e6fc18d25f5bf148a6b26d76a6da777b20015b3b
Jackson <Jackson.harris3@gmail.com>
2020-08-06 18:00:38 -0700

    update tracking componenets

--- Components/Tracking.js
+++ Components/Tracking.js
@@ -35,14 +35,14 @@ const Tracking = () => {
         `,
 					}}
 				/>
-				<noscript>
+				{/* <noscript>
 					<img
 						height="1"
 						width="1"
 						style="display:none"
 						src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"
 					/>
-				</noscript>
+				</noscript> */}
 			</Head>
 		</div>
 	);
