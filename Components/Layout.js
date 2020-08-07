f0cb2725ce46efcf6486acbb32b68075135a3e7f
Jackson <Jackson.harris3@gmail.com>
2020-08-06 17:54:11 -0700

    init tracking componenet

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import Navigation from './Navigation';
 import Footer from './Footer';
+import Tracking from './Tracking'
 
 export default function Layout({ children }) {
 	return (
 		<div className="sr-orange2-bg mh-100">
+			<Tracking />
 			<Navigation />
 
 			<main className="pt-5">{children}</main>
