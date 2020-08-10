df93da4d0e0140cb0ff40485640a24df0b99711b
Jackson <Jackson.harris3@gmail.com>
2020-08-10 16:03:52 -0700

    update post image

--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import { RichText, Date } from 'prismic-reactjs';
 import { client } from '../prismic-configuration';
 import Link from 'next/link';
 import Layout from "../Components/Layout"
-import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol,  MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBIcon } from "mdbreact";
+import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol,  MDBCardImage, MDBCardBody, MDBCardTitle, MDBCardText, MDBIconm, MDBView } from "mdbreact";
 import Moment from "moment"
 
 
@@ -12,30 +12,32 @@ const Post = (props) => {
 	const formattedDate = Moment(date).format('LL');
 
 	return (
-	
 		<Layout>
-		<MDBContainer className="mt-5">
-			{/* <Link href="/">
+			<MDBContainer className="mt-5">
+				{/* <Link href="/">
 				<MDBIcon icon="backward" />
 			</Link> */}
-			<MDBRow className="">
-				<MDBCol md="8" className="d-flex justify-content-center mb-4 mx-auto">
-					<img className="img-fluid z-depth-0" src={props.post.data.post_main_image.url} />
-				</MDBCol>
-				<MDBCol lg="10" className="mx-auto">
-					<h1 className="font-playfair-d mb-4 font-weight-bolder">{RichText.render(props.post.data.title)}</h1>
+				<MDBRow className="">
+					<MDBCol md="8" className="d-flex justify-content-center mb-4 mx-auto">
+						<MDBView hover className="rounded z-depth-0 mb-4" waves>
+							<img className="img-fluid z-depth-0 postImage" src={props.post.data.post_main_image.url} />
+						</MDBView>
+					</MDBCol>
+					<MDBCol lg="10" className="mx-auto">
+						<h1 className="font-playfair-d mb-4 font-weight-bolder">{RichText.render(props.post.data.title)}</h1>
 
-					<h2>{RichText.render(props.post.data.author)}</h2>
+						<h2>{RichText.render(props.post.data.author)}</h2>
 
-					<span>{formattedDate}</span>
-					
-					<br/><br/>
-					{RichText.render(props.post.data.post_body)}
-				</MDBCol>
-			</MDBRow>
-		</MDBContainer>
+						<span>{formattedDate}</span>
+
+						<br />
+						<br />
+						{RichText.render(props.post.data.post_body)}
+					</MDBCol>
+				</MDBRow>
+			</MDBContainer>
 		</Layout>
-	)	
+	);	
 };
 
 Post.getInitialProps = async (context) => {
